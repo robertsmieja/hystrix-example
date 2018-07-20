6e52ee9cfcf2afdde463e820d389431638ff1856
Robert Smieja <robertsmieja@gmail.com>
2018-07-20 00:57:57 -0400

    Initial commit

---
+++ src/main/java/com/robertsmieja/examples/hystrix/HystrixExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.robertsmieja.examples.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixExamplesApplication.class, args);
+	}
+}
