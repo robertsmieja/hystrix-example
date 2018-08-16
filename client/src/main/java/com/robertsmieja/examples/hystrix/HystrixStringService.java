06bf851136aa3b33a13a548aee01857aee73c112
Robert Smieja <robertsmieja@gmail.com>
2018-08-16 01:47:25 -0400

    Some more cleanup

--- client/src/main/java/com/robertsmieja/examples/hystrix/HystrixStringService.java
+++ client/src/main/java/com/robertsmieja/examples/hystrix/HystrixStringService.java
@@ -9,6 +9,7 @@ import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.HystrixCommand;
 import com.robertsmieja.examples.hystrix.commands.FaultyStringClientCommand;
 import com.robertsmieja.examples.hystrix.commands.StringClientCommand;
 
@@ -27,29 +28,24 @@ public class HystrixStringService {
         StringBuffer sb = new StringBuffer();
         StringClientCommand stringClientCommand = new StringClientCommand(stringServiceClient, world);
 
-        Instant start = Instant.now();
-        String result = stringClientCommand.execute();
-        Instant end = Instant.now();
-        long durationMs = ChronoUnit.MILLIS.between(start, end);
-
-        sb.append("Result - ").append(result).append(System.lineSeparator());
-        sb.append("Start - ").append(start).append(System.lineSeparator());
-        sb.append("End - ").append(end).append(System.lineSeparator());
-        sb.append("Duration (ms) - ").append(durationMs).append(System.lineSeparator());
-
-        return sb.toString();
+        return printCommandStats(stringClientCommand);
     }
 
     @ShellMethod("Hystrix FaultyStringService")
     public String faultyHystrixHello(@ShellOption(defaultValue = "World") String world){
-        StringBuffer sb = new StringBuffer();
         FaultyStringClientCommand stringClientCommand = new FaultyStringClientCommand(stringServiceClient, world);
 
+        return printCommandStats(stringClientCommand);
+    }
+
+    private String printCommandStats(HystrixCommand<String> stringClientCommand){
+        StringBuffer sb = new StringBuffer();
         Instant start = Instant.now();
         String result = stringClientCommand.execute();
         Instant end = Instant.now();
         long durationMs = ChronoUnit.MILLIS.between(start, end);
 
+        sb.append("Hystrix Circuit Breaker Open? - ").append(stringClientCommand.isCircuitBreakerOpen()).append(System.lineSeparator());
         sb.append("Result - ").append(result).append(System.lineSeparator());
         sb.append("Start - ").append(start).append(System.lineSeparator());
         sb.append("End - ").append(end).append(System.lineSeparator());
@@ -57,5 +53,4 @@ public class HystrixStringService {
 
         return sb.toString();
     }
-
 }
--- server/src/main/java/com/robertsmieja/examples/hystrix/HystrixServerApplication.java
+++ server/src/main/java/com/robertsmieja/examples/hystrix/HystrixServerApplication.java
@@ -6,17 +6,14 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.EnableMBeanExport;
 
-import com.netflix.hystrix.contrib.servopublisher.HystrixServoMetricsPublisher;
-import com.netflix.hystrix.strategy.HystrixPlugins;
-
 @EnableMBeanExport
 @EnableHystrix
 @EnableHystrixDashboard
 @SpringBootApplication
-public class HystrixExamplesApplication {
+public class HystrixServerApplication {
 
     public static void main(String[] args) {
 //        HystrixPlugins.getInstance().registerMetricsPublisher(HystrixServoMetricsPublisher.getInstance()); //Add metrics to JMX
-        SpringApplication.run(HystrixExamplesApplication.class, args);
+        SpringApplication.run(HystrixServerApplication.class, args);
     }
 }
